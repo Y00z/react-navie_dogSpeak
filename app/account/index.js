a1fd5a1a7251c2fb05fe9086cc0ea3ffaf44104f
y00z <550823517@qq.com>
2017-03-23 16:29:53 +0800

    上拉加载更多

--- app/account/index.js
+++ app/account/index.js
@@ -11,19 +11,27 @@ import {
     View,
     Image,
     Navigator,
-    ListView
+    ListView,
+    ActivityIndicator
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 var Dimensions = require("Dimensions");
 var {width, height} = Dimensions.get('window');
 var Mock = require('mockjs');
+var cacheResult = {
+    nextPage: 1,
+    items: [],
+    total: 0
+}
 export default class Account extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             dataSource: new ListView.DataSource({
                 rowHasChanged: (r1, r2) => r1 !== r2
-            })
+            }),
+            isMoreLoading: false    //是否正在加载更多数据
         }
     }
 
@@ -33,43 +41,89 @@ export default class Account extends Component {
 
     render() {
         return (
-            <View style={{backgroundColor: 'rgba(240,239,245,1.0)'}}>
+            <View style={{backgroundColor: 'rgba(240,239,245,1.0)', flex: 1}}>
                 <View style={styles.accountTopStyles}>
                     <Text style={styles.accountTopTextStyles}>狗狗说</Text>
                 </View>
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={this._renderRow}
+                    automaticallyAdjustContentInsets={false}
+                    enableEmptySections={true}
+                    onEndReached={() => this._fetchMore()}   //加载更多
+                    onEndReachedThreshold={20}
+                    renderFooter={() => this._renderFooter()}
+                    showsVerticalScrollIndicator={false}    //隐藏纵向滚动条
                 />
             </View>
         )
     }
 
-    componentDidMount() {
-        this.initData()
+    _renderFooter() {       //全部加载完毕的时候。
+        if (!this._hasMore() && cacheResult.total !== 0) {
+            return <View>
+                <Text style={{fontSize: 15, color: 'gray', textAlign: 'center'}}>已经没有更多数据了</Text>
+            </View>
+        }
+
+        if (!this.state.isMoreLoading) {
+            return <View></View>
+        }
+
+        return <ActivityIndicator style={{height: 80}} size="small"/>
     }
 
-    initData() {
-        fetch(this.props.uri_api)
+    _hasMore() {     //当视频总量，等于  已经缓存了的视频总量
+        if (cacheResult.total <= cacheResult.items.length) {
+            return false
+        } else {    //当视频总量，不等于  已经缓存了的视频总量，说明还有数据
+            return true;
+        }
+    }
+
+    _fetchMore() {     //加载更多，如果没有了更多数据，或者正在加载数据的状态中，就return
+        if (!this._hasMore() || this.state.isMoreLoading) {
+            return null;
+        }
+        var page = cacheResult.nextPage;
+        this.initData(page)
+    }
+
+
+    componentDidMount() {
+        this.initData(1)
+    }
+
+    initData(page) {
+        this.setState({
+            isMoreLoading: true
+        })
+        fetch(this.props.uri_api + "&page=" + page)
             .then((response) => response.json())
             .then((response) => {
                 var data = Mock.mock(response);
-                this.parseData(data.data);
+                this.parseData(data);
             })
             .catch((error) => {
+                this.setState({
+                    isMoreLoading: false
+                });
                 console.error(error)
             })
     }
 
     parseData(data) {
-        var listArr = [];
-        for (var i = 0; i < data.length; i++) {
-            var itemData = data[i];
-            listArr.push(itemData);
-        }
-        this.setState({
-            dataSource: this.state.dataSource.cloneWithRows(listArr)
-        });
+        var item = cacheResult.items.slice();  //把缓存中的item数组放到一个新的数组里面
+        item = item.concat(data.data)           //item里面追加新加载的数据。
+        cacheResult.items = item               //cacheResult里面放着所有加载的数据
+        cacheResult.total = data.total
+        setTimeout(()=> {
+            this.setState({
+                isMoreLoading: false,
+                dataSource: this.state.dataSource.cloneWithRows(cacheResult.items)
+            });
+        }, 800)
+
     }
 
     _renderRow(rowData) {
